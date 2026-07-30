625317406e1a9a8553fcb07dad89d17358a688c7
0Ens <eneserul20@hotmail.com>
2026-07-31 02:26:06 +0300

    test: basarili login e2e testi

---
+++ e2e/login.spec.ts
@@ -0,0 +1,9 @@
+import { test, expect } from '@playwright/test';
+
+test('basarili login sonrasi inventory sayfasina yonlendirir', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page).toHaveURL(/inventory.html/);
+});
