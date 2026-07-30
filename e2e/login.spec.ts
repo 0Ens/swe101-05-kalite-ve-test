b4a1dfc8c3717e6f2b57331b3d83414a347662a5
0Ens <eneserul20@hotmail.com>
2026-07-31 02:33:46 +0300

    test: sepete ekleme e2e testi

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -15,3 +15,12 @@ test('gecersiz bilgilerle giris hata mesaji gosterir', async ({ page }) => {
   await page.locator('[data-test="login-button"]').click();
   await expect(page.locator('[data-test="error"]')).toBeVisible();
 });
+
+test('urun sepete eklendiginde rozette 1 gorunur', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
+});
