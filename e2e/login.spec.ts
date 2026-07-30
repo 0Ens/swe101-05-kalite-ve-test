e47ddcc909eb98efe474ae8b477861f1bf625dfd
0Ens <eneserul20@hotmail.com>
2026-07-31 02:47:07 +0300

    test: checkout akisi e2e testi

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -35,3 +35,19 @@ test('urun sepetten cikarildiginda rozet kayboluyor', async ({ page }) => {
   await page.locator('[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
   await expect(page.locator('[data-test="shopping-cart-badge"]')).not.toBeVisible();
 });
+
+test('checkout akisi basariyla tamamlanir', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').fill('Enes');
+  await page.locator('[data-test="lastName"]').fill('Erul');
+  await page.locator('[data-test="postalCode"]').fill('34000');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await expect(page.locator('[data-test="complete-header"]')).toBeVisible();
+});
