51e2effb13dc8cebca0981c502ec79f43bcaad30
0Ens <eneserul20@hotmail.com>
2026-07-31 02:38:03 +0300

    test: sepetten cikarma e2e testi

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -24,3 +24,14 @@ test('urun sepete eklendiginde rozette 1 gorunur', async ({ page }) => {
   await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
   await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
 });
+
+test('urun sepetten cikarildiginda rozet kayboluyor', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-bolt-t-shirt"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).toHaveText('1');
+  await page.locator('[data-test="remove-sauce-labs-bolt-t-shirt"]').click();
+  await expect(page.locator('[data-test="shopping-cart-badge"]')).not.toBeVisible();
+});
