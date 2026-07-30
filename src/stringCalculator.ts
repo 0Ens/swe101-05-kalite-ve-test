c010f3badc5c48c908eaee3266c9d3d5c7f46434
0Ens <eneserul20@hotmail.com>
2026-07-31 02:12:57 +0300

    green: gecersiz girdi icin hata firlatma

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,5 +3,11 @@ export function add(input: string): number {
     return 0;
   }
   const parts = input.split(",");
-  return parts.reduce((sum, part) => sum + Number(part), 0);
+  return parts.reduce((sum, part) => {
+    const num = Number(part);
+    if (Number.isNaN(num)) {
+      throw new Error(`Geçersiz sayı: "${part}"`);
+    }
+    return sum + num;
+  }, 0);
 }
