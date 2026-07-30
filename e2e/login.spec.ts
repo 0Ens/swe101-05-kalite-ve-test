b26323a39d0381df06ba3e81eda71184ea577b3d
0Ens <eneserul20@hotmail.com>
2026-07-31 02:30:42 +0300

    test: basarisiz login e2e testi

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -7,3 +7,11 @@ test('basarili login sonrasi inventory sayfasina yonlendirir', async ({ page }) 
   await page.locator('[data-test="login-button"]').click();
   await expect(page).toHaveURL(/inventory.html/);
 });
+
+test('gecersiz bilgilerle giris hata mesaji gosterir', async ({ page }) => {
+  await page.goto('/');
+  await page.locator('[data-test="username"]').fill('yanlis_kullanici');
+  await page.locator('[data-test="password"]').fill('yanlis_sifre');
+  await page.locator('[data-test="login-button"]').click();
+  await expect(page.locator('[data-test="error"]')).toBeVisible();
+});
